2cafca04ba02419f07556f61741ad52530094cc9
mdoli <mamsorea@gmail.com>
2022-07-30 21:18:37 +0900

    update

--- ch1/front/components/LoginForm.vue
+++ ch1/front/components/LoginForm.vue
@@ -1,19 +1,21 @@
 <template>
 	<v-container>
 		<v-card>
-			<v-form>
+			<v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
 				<v-container>
 					<v-text-field
 							label="이메일"
 							type="email"
+							:rules="emailRules"
 							required
 					/>
 					<v-text-field
 							label="비밀번호"
 							type="password"
+							:rules="passwordRules"
 							required
 					/>
-					<v-btn color="green" type="submit">로그인</v-btn>
+					<v-btn color="green" type="submit" :disabled="!valid">로그인</v-btn>
 					<v-btn nuxt to="/signup">회원가입</v-btn>
 				</v-container>
 			</v-form>
@@ -23,7 +25,30 @@
 
 <script>
 	export default {
-		name: "LoginFrom"
+		data() {
+			return {
+				valid: false,
+				email: '',
+				password: '',
+				emailRules: [
+					v => !!v || '이메일은 필수입니다.',
+					v => /.+@.+/.test(v) || '이메일이 유효하지 않습니다.',
+				],
+				passwordRules: [
+					v => !!v || '비밀번호는 필수입니다.',
+				],
+			};
+		},
+		methods: {
+			onSubmitForm() {
+				if (this.$refs.form.validate()) {
+					alert('로그인 시도!');
+				} else {
+					alert('폼이 유효하지 않습니다.');
+				}
+			}
+		}
+		
 	}
 </script>
 
